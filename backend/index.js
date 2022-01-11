93b6159aed9fbe1865ab148899d25b2e7f32ec0f
Gustavo Gutierrez <gdgutierrez.19@est.ucab.edu.ve>
2022-01-11 12:25:52 -0600

    Servidor listo
    *Todas las rutas empiezan con /api
    *Se agrego el modulo cors y connect-history-api-fallback
    *Se configuro el servidor para procesar objetos js y json
    *Se configuro la carpeta public en el servidor

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express= require('express');
 const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const history = require('connect-history-api-fallback');
+
 //inicializaciones
 const app= express();
 
@@ -9,15 +13,20 @@ app.set('port', process.env.PORT || 4000);
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 //VARIABLES GLOBALES
 
 //RUTAS
-app.use(require('./routes/index'));
+app.use('/api', require('./routes/index'));
 
 //Public
+app.use(history());
+app.use(express.static(path.join(__dirname, '/public')));
 
 //INICIAR EL SERVIDOR
 app.listen(app.get('port'), () =>{
-    console.log('Servidor en puerto 4000');
+    console.log('Server on port', app.get('port'));
 });
