0a21b2486e7caa90d762e994370ca5de5c4c1b70
gustavo-dgs <gdgutierrez.19@est.edu.ucab.ve>
2022-01-15 19:11:02 -0600

    intento 1 - pantallas Home y Editor

---
+++ client/src/views/Editor.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Encuesta {{$route.params.id}}</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        </form>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="button">NEW</button>
+      </div>
+    </nav>
+
+    <div class="container">
+      
+      <div v-for="i in 5" :key="i" class="card" style="width: 60rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{'# '+ i}} Question title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Question Descrption</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Toolbar from "../components/Toolbar.vue";
+
+export default {
+    components: {
+        //Toolbar
+    }
+}
+</script>
+
+<style>
+  .container {
+    margin: 40px;
+    text-align: left;
+    display: inline-block;
+  }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+
+    <HorizontalNavbar id="horizontal-navbar" />
+
+    <div class="container__2">
+      <VerticalNavbar />
+      <div class="containter__3">
+        <nav class="nav">
+          <a class="nav-link active" href="#">Active</a>
+          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link disabled" href="#">Disabled</a>
+          <button type="button" class="btn btn-primary">Sort</button>
+          <button type="button" class="btn btn-primary">Display</button>
+        </nav>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col">Creation Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="i in 5" :key="i">
+              <router-link :to="/editor/+i">
+                <th scope="row"> {{ i }} </th>
+                <td>Encuesta {{ i }} </td>
+                <td>Mi primera encuesta</td>
+                <td>15/01/2022</td>
+              </router-link>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HorizontalNavbar from "../components/HorizontalNavbar.vue";
+import VerticalNavbar from "../components/VerticalNavbar.vue";
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    HorizontalNavbar,
+    VerticalNavbar,
+  },
+};
 </script>
+
+<style>
+.container__2 {
+  display: inline-flex;
+  /*width: auto;*/
+  margin-left: 0px;
+}
+
+.container__2 table {
+  width: auto;
+  margin: 0px;
+}
+
+.container__2 VerticalNavbar {
+  margin-left: 0;
+  width: 50px;
+}
+
+.container__3 nav {
+  height: 200px;
+}
+</style>
