8e9d5b14d07101760ba5cd42c3d81d998919dd55
gustavo-dgs <gdgutierrez.19@est.edu.ucab.ve>
2022-01-19 01:46:19 -0600

    aomodando main_profile-menu del header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-container">
-    <!-- <Header/> -->
-    <router-view/>
+    <Header class="main-container__header"/>
+    <router-view class="main-container__router-view"/>
   </div>
   
 </template>
@@ -25,4 +25,30 @@ export default {
   }
   #app {
   }
+
+   @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,100&display=swap');
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+    body{
+        background: #2c2525;
+        overflow-x: hidden;
+    }
+    body::-webkit-scrollbar{
+        width: 8px;
+        margin-right: 5px;
+    }
+    body::-webkit-scrollbar-thumb{
+        background: #edeaf1;
+        border-radius: 4px;
+        border-right: 5px solid #2c2525;
+        /* margin-right: 10px; */
+    }
+    body::-webkit-scrollbar-thumb:active {
+        background-color: #d6d3da;
+    }
+
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,67 +1,36 @@
 <template>
-    <div class="home-container">
-        <header class="header">
-            <div class="header__primario">
-                <ion-icon @click="toggle" class="header__primario--menu-icon" name="menu-outline"></ion-icon>
-                <div class="header__primario__logo">
-                    <ion-icon name="newspaper-outline"></ion-icon>
-                    <p>Survea</p>
-                </div>
+    <div class="main">
+            
+                        <!--OJO AQUI :class="{active: mainActive}" -->
+        <section class="main__tablon" >
+            <div class="main__tablon__cabecera">
+                <button>Nuevo</button>
+                <button>Vista</button>
             </div>
-            <div class="header__secundario">
-                <ion-icon name="search-outline"></ion-icon>
-                <button>Mis encuestas</button>
-            </div>
-        </header>
 
-        <article class="main">
-            <section class="main__profile-menu" :class="{active: navegationActive}">
-                <img class="main__profile-menu--imagen" src="../assets/profile/profile_02.jpg" alt="profile_02.jpg">
-                <div class="main__profile-Menu__carrousel">
-                    <ul>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                        <li>Example</li>
-                    </ul>
-                    <p>(C)2022 - UCAB Extension Guayana</p>
-                </div>
-            </section>
-            <section class="main__tablon" :class="{active: mainActive}">
-                <div class="main__tablon__cabecera">
-                    <button>Nuevo</button>
-                    <button>Vista</button>
+
+            <div class="main__tablon__feed">
+                <div class="main__tablon__feed__titulos">
+                    <p>Numero</p>
+                    <p>Descripcion</p>
+                    <p>Fecha</p>
+                    <p>Edit</p>
                 </div>
 
 
-                <div class="main__tablon__feed">
-                    <div class="main__tablon__feed__titulos">
-                        <p>Numero</p>
+                <div class="main__tablon__feed__carrousel">
+                    <div @click="goToEditor(i)" class="element" v-for="i in 5" :key="i">
+                        <p>Encuesta {{ i }}</p>
                         <p>Descripcion</p>
                         <p>Fecha</p>
-                        <p>Edit</p>
-                    </div>
-
-
-                    <div class="main__tablon__feed__carrousel">
-                        <div @click="goToEditor(i)" class="element" v-for="i in 5" :key="i">
-                            <p>Encuesta {{ i }}</p>
-                            <p>Descripcion</p>
-                            <p>Fecha</p>
-                            <ion-icon name="settings-outline"></ion-icon>
-                        </div>
+                        <ion-icon name="settings-outline"></ion-icon>
                     </div>
                 </div>
+            </div>
 
 
-            </section>
-        </article>
+        </section>
+    
     </div>
 </template>
 
@@ -93,140 +62,12 @@
     accent: colors.indigo.base, // #3F51B5 */
 
     /* object-fit: cover; */
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,100&display=swap');
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-    }
-    body{
-        background: #2c2525;
-        overflow-x: hidden;
-    }
-    body::-webkit-scrollbar{
-        width: 8px;
-        margin-right: 5px;
-    }
-    body::-webkit-scrollbar-thumb{
-        background: #edeaf1;
-        border-radius: 4px;
-        border-right: 5px solid #2c2525;
-        /* margin-right: 10px; */
-    }
-    body::-webkit-scrollbar-thumb:active {
-        background-color: #d6d3da;
-    }
-    .header{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        /*  */
-        height: 60px;
-        width: 100%;
-        background: #44974f;
-    }
-    .header__primario{
-        display: flex;
-        align-items: center;
-        margin-left: 10px;
-    }
-    .header__primario--menu-icon{
-        margin-right: 15px;
-        /*  */
-        font-size: 40px;
-        /*  */
-        color: #ffffff;
-    }
-    .header__primario__logo{
-        display: flex;
-        align-items: center;
-        /*  */
-        color: #ffffff;
-    }
-    .header__primario__logo ion-icon{
-        font-size: 30px;
-    }
-    .header__primario__logo p{
-        display: none;
-    }
-    .header__secundario{
-        display: flex;
-        align-items: center;
-        /*  */
-        margin-right: 10px;
-    }
-    .header__secundario ion-icon{
-        margin-right: 10px;
-        /*  */
-        font-size: 30px;
-        color: #fff;
-    }
-    .header__secundario button{
-        padding: 5px;
-        border-radius: 10px;
-        /*  */
-        border: solid #fff;
-        background: transparent;
-        color: #fff;
-    }
-    .header__secundario button:hover{
-        text-decoration: underline 3px;
-        background: #fff;
-        color: #44974f;
-    }
+    
     /* --------------- MAIN --------------- */
     .main{
-        display: flex;
+        /* display: flex; */
     }
-    /* ---- menu ---- */
-    .main__profile-menu{
-        /*  */
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-        /*  */
-        margin-top: 35px;
-        width: 0;
-        /*  */
-        transition: 0.3s;
-        overflow-x: hidden;
-    }
-    .main__profile-menu.active{
-        width: 100%;
-    }
-    .main__profile-menu--imagen{
-        width: 200px;
-        height: 200px;
-        border-radius: 100%;
-        /*  */
-        object-fit: cover;
-        border: solid;
-    }
-    .main__profile-Menu__carrousel{
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        justify-content: flex-start;
-        /*  */
-        margin-top: 25px;
-    }
-    .main__profile-Menu__carrousel ul{
-        margin-bottom: 25px;
-    }
-    .main__profile-Menu__carrousel ul li{
-        margin-bottom: 15px;
-        /*  */
-        color: #fff;
-        list-style: none;
-    }
-    .main__profile-Menu__carrousel ul li:hover{
-        text-decoration: underline 3px;
-    }
-    .main__profile-Menu__carrousel p{
-        color: #ffffff25;
-    }
+    
     /* ---- Tablon ---- */
     .main__tablon{
         display: flex;
