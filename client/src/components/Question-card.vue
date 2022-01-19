da88cbb92406ebf90008a8ad037c3fb6f484401a
gustavo-dgs <gdgutierrez.19@est.edu.ucab.ve>
2022-01-19 03:17:14 -0600

    Empezando vista editor

---
+++ client/src/components/Question-card.vue
@@ -0,0 +1,90 @@
+<template>
+
+  <div class="question-card question_list__question-card">
+        <h2 class="question-card__title">Question title</h2>
+        <hr/>
+        <p class="question-card__text">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+        </p>
+
+        <form v-if="questionType === 'select'" class="question-form question-form_type_select">
+            <select class="input select">
+                <option class="select_opction" value="volvo">Volvo</option>
+                <option class="select_opction" value="saab">Saab</option>
+                <option class="select_opction" value="fiat">Fiat</option>
+                <option class="select_opction" value="audi">Audi</option>
+            </select>
+        </form>
+
+        <form v-else-if="questionType === 'textarea'" class="question-form question-form_type_textarea">
+            <textarea class="input textarea" placeholder="Escriba su respuesta"></textarea>
+        </form>
+
+        <form v-else-if="questionType === 'checkbox'" class="question-form question-form_type_checkbox">
+            <input class="input checkbox" type="checkbox" value="1">
+            <label for="1">1</label><br>
+            <input class="input checkbox" type="checkbox" value="2">
+            <label for="2">2</label><br>
+            <input class="input checkbox" type="checkbox" value="3">
+            <label for="3">3</label><br>
+        </form>
+
+        <form v-else-if="questionType === 'date'" class="question-form question-form_type_date">
+            <input class="input date" type="date">
+        </form>
+
+        <form v-else-if="questionType === 'email'" class="question-form question-form_type_email">
+            <input class="input email" type="email">
+        </form>
+
+        <form v-else-if="questionType === 'number'" class="question-form question-form_type_number">
+            <input class="input number" type="number">
+        </form>
+
+        <form v-else-if="questionType === 'radio'" class="question-form question-form_type_radio">
+            <input class="input radio" name="some_name" type="radio" value="1">
+            <label for="1">1</label><br>
+            <input class="input radio" name="some_name" type="radio" value="2">
+            <label for="2">2</label><br>
+            <input class="input radio" name="some_name" type="radio" value="3">  
+            <label for="3">3</label><br>
+        </form>
+
+        <form v-else-if="questionType === 'text'" class="question-form question-form_type_text">
+            <input class="input text" type="text">
+        </form>
+
+        <form v-else-if="questionType === 'time'" class="question-form question-form_type_time">
+            <input class="input time" type="time">
+        </form>
+
+        <form v-else-if="questionType === 'tel'" class="question-form question-form_type_tel">
+            <input class="input tel" type="tel" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+        </form>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        questionType: String
+    }
+}
+</script>
+
+<style>
+    .question-card {
+        background: #fff;
+        border-radius: 20px;
+        margin: 10px 0px;
+        width: 100%;
+        padding: 20px;
+    }
+
+    .textarea {
+        resize: none;
+    }
+</style>
--- client/src/views/Editor.vue
+++ client/src/views/Editor.vue
@@ -1,73 +1,54 @@
 <template>
-  <div>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Encuesta {{$route.params.id}}</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <div class="editor-container">
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        </form>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="button">NEW</button>
-      </div>
-    </nav>
+    <h1 class="editor-container__title">Encuesta {{ $route.params.id }}</h1>
 
-    <div class="container">
-      
-      <div v-for="i in 5" :key="i" class="card" style="width: 60rem;">
-        <div class="card-body">
-          <h5 class="card-title">{{'# '+ i}} Question title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Question Descrption</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
+    <div class="question_list">
+
+        <question-card v-for="type of questionArr" :key="type" :questionType="type"/>
 
     </div>
 
   </div>
+
 </template>
 
 <script>
-import Toolbar from "../components/Toolbar.vue";
+import QuestionCard from '../components/Question-card.vue';
 
 export default {
+    data() {
+        return {
+            questionType: 'checkbox',
+            questionArr: ['checkbox', 'radio', 'text', 'date', 'textarea']
+        }
+    },
     components: {
-        //Toolbar
+        'question-card': QuestionCard
     }
-}
+};
 </script>
+        QuestionCard
 
 <style>
-  .container {
-    margin: 40px;
-    text-align: left;
-    display: inline-block;
-  }
+    .editor-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+    }
+
+    .editor-container__title {
+        color: white;
+    }
+
+    .question-list {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: flex-start;
+        max-width: 500px;
+    }
+    
 </style>
