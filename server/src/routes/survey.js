3d2233a5238c66256b6c6ead523a52d116005e85
Yohangel <yohangel.lopez.yl@gmail.com>
2022-01-21 19:08:19 -0400

    survey

--- server/src/routes/survey.js
+++ server/src/routes/survey.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const db = require('../database');
 
-router.get('/survey', (req,res) =>{
+router.get('/', (req,res) =>{
     db.query('SELECT * FROM Survey', (err, rows, fields) =>{
         if(!err){
             res.json(rows);
@@ -13,7 +13,7 @@ router.get('/survey', (req,res) =>{
     })
 });
 
-router.get('/survey/:id', (req,res) =>{
+router.get('/:id', (req,res) =>{
     const {id} = req.params;
     db.query('SELECT sur.*, ques.*, as.* FROM Survey as sur,Question as ques, Answer as ans WHERE sur.ID_Survey = ? AND sur.ID_Survey= ques.ID_Survey AND ',[id], (err, rows, fields) =>{
         if(!err){
@@ -24,7 +24,7 @@ router.get('/survey/:id', (req,res) =>{
     })
 });
 
-router.post('/survey', (req,res) =>{
+router.post('/', (req,res) =>{
     const {} = req.body;
     db.query('INSERT INTO Survey() VALUES ',[], (err,rows,fields) =>{
         if(!err){
@@ -35,7 +35,7 @@ router.post('/survey', (req,res) =>{
     })
 })
 
-router.put('/survey/:id', (req,res)=> {
+router.put('/:id', (req,res)=> {
     const {id} = req.params;
     const {} = req.body;
     db.query('UPDATE Survey SET ',[], (err,rows,fields) =>{
